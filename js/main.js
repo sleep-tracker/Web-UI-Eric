1c2ca1ef10390de0b25974802d911435f59b09ba
Eric Nunn <lowlyherotech@gmail.com>
2019-04-25 17:18:44 -0400

    Dropdown functional on all pages. Just need to add event listeners for MVP and then webpage is complete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const navItems = document.querySelector('.nav-item .nav-link');
+// const navItems = document.querySelector('.nav-item .nav-link');
 
-navItems.addEventListener('mouseover', (event) => {
-    event.target.style.transition = 'transform .3s';
-    event.target.style.transform = 'scale(1.15)';
-});
-navItems.addEventListener('mouseout', (event) => {
-    event.target.style.transition = 'transform .3s';
-    event.target.style.transform = 'scale(1.0)';
-});
+// navItems.addEventListener('mouseover', (event) => {
+//     event.target.style.transition = 'transform .3s';
+//     event.target.style.transform = 'scale(1.15)';
+// });
+// navItems.addEventListener('mouseout', (event) => {
+//     event.target.style.transition = 'transform .3s';
+//     event.target.style.transform = 'scale(1.0)';
+// });
